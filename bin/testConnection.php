3b3cbc6c05748f337edb03343a8ff274502ee74d
Dwi Sasongko S <ruckuus@gmail.com>
2014-04-19 06:57:09 +0000

    Cleanup

--- bin/testConnection.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-require dirname(__FILE__) . '/../vendor/autoload.php';
-$server = "web1-staging.guruestate.com";
-$c = new YouPloy\Connection($server, 22, 'dwi','/home/vagrant/.ssh/id_rsa');
-echo $c->cmd('pwd');
-echo $c->cmd('ls /var/www/propertyguru.com.sg');
